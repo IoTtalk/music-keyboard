b750882a385354112bd40a4148c10113ccf5cfca
knockbuster <knockbusterx@hotmail.com>
2016-07-11 01:53:07 +0800

    init

---
+++ server.js
@@ -0,0 +1,23 @@
+
+var express = require("express"),
+	app = express(),
+	server = require('http').createServer(app),
+	serv_io = require('socket.io')(server);
+
+app.use(express.static("./webapp"));
+server.listen((process.env.PORT || 5000));
+
+
+serv_io.sockets.on('connection', function(socket) {
+    socket.on('join', function(data) { socket.join(data); })
+    socket.on('leave', function(data) { socket.leave(data); })
+});
+
+var room = 'red'
+setInterval(function(){
+    serv_io.sockets.in(room).emit('message', 'hello world!');
+}, 1000);
+
+
+
+
