4aa8c1d26dd39213551b51353875d7275bff1888
knockbuster <knockbusterx@hotmail.com>
2016-07-22 04:42:41 +0800

    make it better

--- server.js
+++ server.js
@@ -1,122 +1,143 @@
-
 var express = require("express"),
-	app = express(),
-	server = require("http").createServer(app),
-	serv_io = require("socket.io")(server),
+    app = express(),
+    server = require("http").createServer(app),
+    serv_io = require("socket.io")(server),
     fs = require("fs"),
     MidiConvert = require("./MidiConvert");
 
 
 app.use(express.static("./webapp"));
 
-app.get("/",function(req,res){
-	res.sendFile(__dirname+'/webapp/html/index.html');
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/webapp/html/index.html');
 });
-app.get("/chooseSong",function(req,res){
-    res.sendFile(__dirname+'/webapp/html/chooseSong.html');
+app.get("/chooseSong", function (req, res) {
+    res.sendFile(__dirname + '/webapp/html/chooseSong.html');
 });
-app.get("/toneDemo",function(req,res){
-    res.sendFile(__dirname+'/webapp/html/toneDemo.html');
+app.get("/toneDemo", function (req, res) {
+    res.sendFile(__dirname + '/webapp/html/toneDemo.html');
 });
-app.get("/temp",function(req,res){
-    res.sendFile(__dirname+'/webapp/html/temp.html');
-});
-server.listen((process.env.PORT || 5566),'0.0.0.0');
 
-var color = ['#dc143c','#ffa500','#ffd700','#3cb371','#1e90ff','#00bfff','#9932cc'];
-var joined = new Array(color.length);
-var part = null;
+server.listen((process.env.PORT || 5566), '0.0.0.0');
+
+var color = ['#dc143c', '#ffa500', '#ffd700', '#3cb371', '#1e90ff', '#00bfff', '#9932cc'];
+var joined = Array.apply(null, Array(color.length)).map(Number.prototype.valueOf,0);
+var song = null;
 var playing = false;
 var head = 0;
-var duration = 50;
+var duration = 20;
 var room = 0;
-var room_num = 2;
+var roomNum = 2;
+var track = 0;
 
-var partition = function(len){
+var partition = function (len) {
     var r;
-    if(head < len){
-        if(head+20 < len){
-            r = {"start":head,"end":head+duration};
-            head+=duration;
+    if (head < len) {
+        if (head + 20 < len) {
+            r = {"start": head, "end": head + duration};
+            head += duration;
         }
-        else{
-            r = {"start":head,"end":len};
+        else {
+            r = {"start": head, "end": len};
             head = len;
         }
         console.log(r);
         return r;
     }
-    else{
+    else {
         return null;
     }
-}
-var sendNotes = function(){
-    var r = partition(part.length);
-    if(r != null){
-        serv_io.sockets.in(color[room%room_num]).emit('part', part.slice(r.start,r.end)); 
-        room++;
+};
+var sendNotes = function () {
+    if (song != null){
+        var part = song.songPart;
+        var r = partition(part.length);
+        if (r != null) {
+            var o = {songPart: part.slice(r.start, r.end), songId: song.songId};
+            serv_io.sockets.in(color[room % roomNum]).emit('partObject', o);
+            room++;
+            playing = true;
+        }
+        else {
+            reset();
+            console.log('reset');
+        }
     }
-    else{
-        reset();
-    }
-}
-var reset = function(){
+};
+var reset = function () {
     playing = false;
     head = 0;
     room = 0;
-}
-var allIn = function(){
-	for(var i = 0; i < room_num; i++)
-    		if(joined[i] != 1)
-    			return false;
+    song = null;
+};
+var allIn = function () {
+    for (var i = 0; i < roomNum; i++)
+        if (joined[i] == 0)
+            return false;
     return true;
-}
-serv_io.sockets.on('connection', function(socket) {
+};
+serv_io.sockets.on('connection', function (socket) {
 
 
-    socket.on('midi',function(songName){
-        fs.readFile("midi/"+songName+".mid", "binary", function(err, data){
-            if (!err){
-                part = MidiConvert.parseParts(data)[0];
-                serv_io.sockets.emit('midi',part);
-            }else{
+    socket.on('midi', function (songName) {
+        fs.readFile("midi/" + songName + ".mid", "binary", function (err, data) {
+            if (!err) {
+                var part = MidiConvert.parseParts(data)[0];
+                serv_io.sockets.emit('midi', part);
+            } else {
                 console.log(err)
             }
         });
-    })
-    socket.on('chooseSong',function(songName){
-        fs.readFile("midi/"+songName+".mid", "binary", function(err, data){
-            part = MidiConvert.parseParts(data)[0];
-            if (!err && allIn()){
+    });
+    socket.on('chooseSong', function (songObject) {
+        fs.readFile("midi/" + songObject.songName + ".mid", "binary", function (err, data) {
+
+
+            if (!err && allIn()) {
                 reset();
+                var part = MidiConvert.parseParts(data)[track];
+
+                //add unique id for each note in part
+                for (var i = 1; i <= part.length; i++)
+                    part[i-1].index = i;
+
+                song = {songPart: part, songId: songObject.songId};
                 sendNotes();
-            }else{
+            }
+            if (err) {
                 console.log(err);
             }
         });
     });
-    socket.on('join', function(data) { 
-		socket.join(data); 
-    	joined[color.indexOf(data)] = 1; 
-    	if(allIn() && !playing){
-            if(!part){  
-                serv_io.sockets.emit('command',"chooseSong");
+    socket.on('join', function (data) {
+        socket.join(data);
+        joined[color.indexOf(data)]++;
+        if (allIn() && !playing) {
+            if (!song) {
+                serv_io.sockets.emit('command', "chooseSong");
             }
-            else{
-                sendNotes(); 
+            else {
+                sendNotes();
             }
-            playing = true;
         }
     });
-    socket.on('leave', function(data) {
-    	socket.leave(data);
-
+    socket.on('leave', function (data) {
+        console.log('leave');
+        socket.leave(data);
+        reset();
+        if(joined[color.indexOf(data)] > 0)
+            joined[color.indexOf(data)]--;
+        for(var i = 0; i < joined.length; i++){
+            if(joined[i] != 0)
+                serv_io.sockets.in(color[i]).emit("command","clearAllPart");
+        }
     });
-    socket.on('ack',function(msg){
-    	if(msg == "played"){ 
+    socket.on('ack', function (msg) {
+        if (msg == "played") {
             sendNotes();
-    	}
+        }
     });
+
 });
 
 
